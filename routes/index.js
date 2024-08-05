012f246103beeebfebc68f3353df3bf88d2049c7
dwd0258 <dwd0258@naver.com>
2024-08-05 17:12:37 +0900

    24.08.05

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 var path = require('path') //경로설정
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-router.get('/test', function(req, res, next){
-  res.sendFile(path.join(__dirname, '../views/test.html'))
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../views/index.html'))
+})
+
+router.get('/login', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../views/members/login.html'))
 })
 
 module.exports = router;
